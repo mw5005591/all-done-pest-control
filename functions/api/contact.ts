ee2bdd753763ebe88457d6f60126b6edc12baa20
Mayer <mw5005591@gmail.com>
2026-04-17 01:02:44 -0400

    try again

--- functions/api/contact.ts
+++ functions/api/contact.ts
@@ -12,8 +12,7 @@ export const onRequestPost = async (context: any) => {
       },
       body: JSON.stringify({
         from: "All Done Pest Control <onboarding@resend.dev>",
-        to: ["abedaskal@gmail.com"],
-        cc: ["mw5005591@gmail.com"],
+        to: ["mw5005591@gmail.com"],
         subject: `New Quote Request: ${firstName} ${lastName}`,
         html: `
           <h2>New Quote Request</h2>
